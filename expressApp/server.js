3a22d4c04cde8a04955940f2d26fcc1caaa936e3
Shiba Ratna Tamrakar <shibaratna@gmail.com>
2015-01-30 13:47:45 +0545

    node express app with get and post router with jaid.

--- expressApp/server.js
+++ expressApp/server.js
@@ -4,14 +4,40 @@ Date: Jan 30, 2015
 Script: Express sample file (basic)
 */
 
+//npm install express
+//npm install body-parser
+
 
 //include express module
 var express = require('express');
-	app = express();
+var bodyParser = require('body-parser');
+var app = express();
 
+
+//using bodyParser
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+var names=[];
+//Get request
 //calling / (root route) with get method and return 'hello world'
+
+app.all('/',function(req,res,next){
+	console.log('from all');
+	next(); // calls next router
+
+});
+
 app.get('/',function(req,res){
-	res.send('hello world');
+	res.render('index.jade',{ names: names });
+
+});
+
+//post
+app.post('/',function(req,res){
+	names.push(req.body.name);
+	res.redirect('/');
 
 });
 
